8e950c963ba72fe6654bd4384dae7e3cee9a983d
Henzo Bragas <103905373+HenzoBragas@users.noreply.github.com>
2024-05-03 20:58:58 -0300

    Create

---
+++ Lista 3 /Exemplo de Matriz 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int linha, coluna, matriz[2][4];
+    
+    for(linha = 0 ; linha < 2; linha++){
+        for(coluna = 0; coluna <4; coluna++){
+            printf("Digite valores de 0 a 100:");
+            scanf("%d", &matriz[linha][coluna]);
+        }
+    }
+    
+    for(linha = 0; linha <2 ; linha++){
+        for(coluna = 0; coluna < 4 ; coluna++){
+            printf("%d\n", matriz[linha][coluna]);
+        }
+        printf("\n");
+    }
+   
+    return 0; 
+}
