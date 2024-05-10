33aa5ecb460d5cc778d09fad00f7e08855d5a851
Henzo Bragas <103905373+HenzoBragas@users.noreply.github.com>
2024-05-10 16:11:25 -0300

    Create 7.1.c

---
+++ Lista 3 /Matriz/7.1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+ 
+    printf("Original matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    int temp[3];
+    for (int j = 0; j < 3; j++) {
+        temp[j] = matrix[0][j];
+        matrix[0][j] = matrix[1][j];
+        matrix[1][j] = temp[j];
+    }
+
+    printf("Modified matrix:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
