be791b8aa316f50ff0eae59df9e3db9fe82103c6
Henzo Bragas <103905373+HenzoBragas@users.noreply.github.com>
2024-05-19 18:14:23 -0300

    Update 5.c

--- Lista 3 /Vetores/5.c
+++ Lista 3 /Vetores/5.c
@@ -3,13 +3,12 @@
 int main() {
     int vetor[15], i, j, min, temp;
 
-    // Lê os elementos do vetor
     printf("Digite 15 elementos para o vetor:\\n");
     for(i = 0; i < 15; i++) {
         scanf("%d", &vetor[i]);
     }
 
-    // Ordenação por seleção
+
     for(i = 0; i < 14; i++) {
         min = i;
         for(j = i + 1; j < 15; j++) {
@@ -24,7 +23,6 @@ int main() {
         }
     }
 
-    // Imprime o vetor ordenado
     printf("Vetor ordenado em ordem crescente:\\n");
     for(i = 0; i < 15; i++) {
         printf("%d ", vetor[i]);
