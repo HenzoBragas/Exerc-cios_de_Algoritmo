cdb241b3c2924afaa3bb764bb85c4a87db516526
Henzo Bragas <103905373+HenzoBragas@users.noreply.github.com>
2024-11-02 13:05:44 -0300

    Create StructComFunção.cpp

---
+++ 2°Semestre/Struct/StructComFunção.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+typedef struct{
+	float Peso;
+	int Idade;
+	float Altura;
+} Pessoa;
+
+Pessoa SetPessoa(int idade, float peso, float altura) {
+	Pessoa P;
+	P.Idade = idade;
+	P.Peso = peso;
+	P.Altura = altura;
+	return P;
+}
+
+void ImprimePessoa(Pessoa P){
+	printf("Idade: %d Peso: %.2f Altura: %.2f\n", P.Idade, P.Peso, P.Altura);
+}
+
+int main(){
+	Pessoa Joao;
+	
+	
+	Joao = SetPessoa(15, 60.5,1.75);
+	ImprimePessoa(Joao);
+	return 0;
+}
